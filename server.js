69b4844c939bf16f54d6e331e9a748b8e733844a
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2022-09-14 12:50:13 -0300

    agrego swagger con el post, get de un user y todos

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require("cors");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 app.use(
@@ -11,6 +13,20 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Users Microservice",
+    description: "API del microservicio de usuarios",
+    version: "0.1",
+  },
+  servers: [{ url: "http://localhost:5001", description: "" }]
+};
+
+const options = { swaggerDefinition, apis: ["./docs/**/*.yaml"] };
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "hello world" });
@@ -20,8 +36,6 @@ const db = require("./src/sequelize");
 db.startConfig(app);
 require("./src/routes/UsersRoutes")(app);
 
-app.listen(5000, () => {
-  console.log('is connected');
-});
+app.listen(5000, () => console.log('is connected'));
 
 module.exports = app;
