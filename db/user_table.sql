c1e5b14390213be586fac37d1f61a85352f83f75
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2022-12-04 16:09:36 -0300

    Fix

--- db/user_table.sql
+++ db/user_table.sql
@@ -10,6 +10,8 @@ CREATE TABLE "Users"(
     total_score INT NOT NULL,
     is_blocked BOOLEAN NOT NULL,
     default_address VARCHAR(500),
+    default_latitude float(53),
+    default_longitude float(53),
     created_at date NOT NULL
 );
 
@@ -40,15 +42,12 @@ CREATE TABLE "Roles"(
 -- INSERT INTO Public."Roles"("id", "name", "description") VALUES (2, 'admin', 'admin')
 -- INSERT INTO Public."Roles"("id", "name", "description") VALUES (3, 'user', 'user')
 -- INSERT INTO Public."Roles"("id", "name", "description") VALUES (4, 'driver', 'driver')
-
 -- INSERT INTO Public."Users"("id", "name", "lastname", "phone_number", "email", "password", "role_id", "number_of_scores", "total_score", "is_blocked", "default_address", "created_at") VALUES ('superadmin', 'superadmin', 123, 'superadmin@fiuber.com', '1234', 1, 0, 0, false, 'calle prueba123', '2020-10-10')
 -- INSERT INTO Public."Users"("id", "name", "lastname", "phone_number", "email", "password", "role_id", "number_of_scores", "total_score", "is_blocked", "default_address", "created_at") VALUES ('admin', 'admin', 123, 'admin@fiuber.com', '1234', 2, 0, 0, false, 'calle prueba123', '2020-10-10')
 -- INSERT INTO Public."Users"("id", "name", "lastname", "phone_number", "email", "password", "role_id", "number_of_scores", "total_score", "is_blocked", "default_address", "created_at") VALUES ('user', 'user', 123, 'user@fiuber.com', '1234', 3, 0, 0, false, 'calle prueba123', '2020-10-10')
 -- INSERT INTO Public."Users"("id", "name", "lastname", "phone_number", "email", "password", "role_id", "number_of_scores", "total_score", "is_blocked", "default_address", "created_at") VALUES ('user', 'blocked', 123, 'blockeduser@fiuber.com', '1234', 3, 0, 0, true, 'calle prueba123', '2020-10-10')
 -- INSERT INTO Public."Users"("id", "name", "lastname", "phone_number", "email", "password", "role_id", "number_of_scores", "total_score", "is_blocked", "default_address", "created_at") VALUES ('driver', 'driver', 123, 'driver@fiuber.com', '1234', 4, 0, 0, false, 'calle prueba123', '2020-10-10')
-
 -- INSERT INTO Public."Drivers"("id", "user_id", "license", "model", "license_plate", "number_of_scores", "total_score") VALUES (5, 'asd', 'dsa', 'dsa', 0, 0)
-
 -- INSERT INTO Public."Reports"("id", "user_id", "driver_id", "description") VALUES (1, 3, 1, 'descripcion 1')
 -- INSERT INTO Public."Reports"("id", "user_id", "driver_id", "description") VALUES (2, 3, 1, 'descripcion 2')
 -- INSERT INTO Public."Reports"("id", "user_id", "driver_id", "description") VALUES (3, 3, 1, 'descripcion 3')
